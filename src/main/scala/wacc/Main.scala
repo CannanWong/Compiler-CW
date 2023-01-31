beffe2743d79841dde4a49f0ca880e23a0313174
CannanWong <wongcannan@gmail.com>
2023-01-31 18:01:33 +0000

    [gl721, tcw121, hww21] Consumed whitespace and comments at start of file, completed symbol implicits, passing skip.wacc

--- src/main/scala/wacc/Main.scala
+++ src/main/scala/wacc/Main.scala
@@ -4,14 +4,40 @@ import parsley.{Parsley, Success, Failure}
 import parsley.character.digit
 import parsley.expr.chain
 import parsley.implicits.character.charLift
+import parsley.token.{Lexer, descriptions, predicate}
+import descriptions.{LexicalDesc, SpaceDesc, SymbolDesc}
 
 import scala.io.Source
 
+object lexer {
+    val desc = LexicalDesc.plain.copy(
+        spaceDesc = SpaceDesc.plain.copy(
+            commentLine = "#",
+            space = predicate.Basic(Character.isWhitespace)
+        ),
+        symbolDesc = SymbolDesc.plain.copy(
+            hardKeywords = Set("begin", "skip", "end")
+        )
+    )
+    //private val comment = symbol("#") *> manyUntil(item, endOfLine)
+    //private val skipWhitespace = skipMany(whitespace <|> comment).hide
+    val lexer = new Lexer(desc)
+
+    def fully[A](p: Parsley[A]): Parsley[A] = lexer.fully(p)//skipWhitespace ~> p <~ eof 
+
+    //def symbol(s: String): Parsley[Unit] = 
+    //    lexer.lexeme.symbol(s)
+    val implicits = lexer.lexeme.symbol.implicits
+}
+
 object Main {
+    import lexer.fully
+    import lexer.implicits.implicitSymbol
+
     def main(args: Array[String]): Unit = {
         println("Hello WACC_45!")
 
-        val fileContents = Source.fromFile(args(0)).getLines.mkString("\n")
+        val fileContents = Source.fromFile(args(0)).getLines().mkString("\n")
         println("File contents:")
         println(fileContents)
 
@@ -25,8 +51,12 @@ object Main {
                 ('(' ~> expr <~ ')') <|> integer,
                 ('+' #> add) <|> ('-' #> sub)
             )
+        
+        lazy val prog: Parsley[Unit] = "begin" ~> "skip" ~> "end"
 
-        expr.parse(fileContents) match {
+        val parser = fully(prog)
+
+        parser.parse(fileContents) match {
             case Success(x) => println(s"${fileContents} = $x")
             case Failure(msg) => println(msg)
         }
