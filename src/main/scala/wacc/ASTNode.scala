f87777fbc9dd008b24b4dfb2cad71110ccf89368
Tsz Wong <tcw121@aube.doc.ic.ac.uk>
2023-02-02 16:38:12 +0000

    Added all AST Node Classes in ASTNode.scala according to WACC BNF

--- src/main/scala/wacc/ASTNode.scala
+++ src/main/scala/wacc/ASTNode.scala
@@ -1,20 +1,97 @@
 package wacc
 
-//class ProgramNode(funcList: List[FuncNode], stat: StatNode) 
-case class ProgramNode(stat:StatNode)
+case class ProgramNode(funcList: List[FuncNode], stat: StatNode) 
 
-//class FuncNode(t: TypeNode, ident: IdentNode, paramList: paramListNode, stat: StatNode)
+case class FuncNode(ty: TypeNode, ident: IdentNode, paramList: ParamListNode, stat: StatNode)
 
+case class ParamListNode(paramList: List[ParamNode])
+
+case class ParamNode(ty: TypeNode, ident: IdentNode)
+
+// StatNode
 sealed trait StatNode
 
 case class SkipNode() extends StatNode
 
-case class StatJoinNode(stats: List[StatNode]) extends StatNode
+case class AssignIdentNode(ty: TypeNode, ident: IdentNode, rvalue: RValueNode) extends StatNode
 
-case class IfNode() extends StatNode
+case class ValuesEqualNode(lvalue: LValueNode, rvalue: RValueNode) extends StatNode
 
-//class LValueNode(ident: IdentNode, arrayElem: ArrayElemNode, pairElem: PairElemNode)
+case class ReadNode(lvalue: LValueNode) extends StatNode
 
-//class PairElemNode(fstLValue: LValueNode, sndLValue: LValueNode)
+case class FreeNode(expr: ExprNode) extends StatNode
 
-//class RValueNode(ident: IdentNode, arrayElem: ArrayElemNode, pairElem: PairElemNode)
+case class ReturnNode(expr: ExprNode) extends StatNode
+
+case class ExitNode(expr: ExprNode) extends StatNode
+
+case class PrintNode(expr: ExprNode) extends StatNode
+
+case class PrintlnNode(expr: ExprNode) extends StatNode
+
+case class IfNode(expr: ExprNode, fstStat: StatNode, sndStat: StatNode) extends StatNode
+
+case class WhileNode(expr: ExprNode, stat: StatNode) extends StatNode
+
+case class BeginEndNode(stat: StatNode) extends StatNode
+
+case class StatJoinNode(statList: List[StatNode]) extends StatNode
+
+// LValueNode
+sealed trait LValueNode
+
+case class IdentNode(name: String) extends LValueNode with ExprNode
+
+case class ArrayElemNode(ident: IdentNode, exprList: List[ExprNode]) extends LValueNode with ExprNode
+
+sealed trait PairElemNode extends LValueNode with RValueNode
+
+case class FstNode(expr: ExprNode) extends PairElemNode 
+
+case class SndNode(expr: ExprNode) extends PairElemNode 
+
+// RValueNode
+sealed trait RValueNode
+
+sealed trait ExprNode extends RValueNode
+
+case class ArrayLiterNode(expr: ExprNode, exprList: List[ExprNode]) extends ExprNode
+
+case class NewPairNode(fstExpr: ExprNode, sndExpr: ExprNode) extends RValueNode
+
+case class CallNode(ident: IdentNode, argList: ArgListNode) extends RValueNode 
+
+case class ArgListNode(exprList: List[ExprNode])
+
+sealed trait TypeNode
+
+case class BaseTypeNode(ty: String) extends TypeNode with PairElemTypeNode
+
+case class ArrayTypeNode(ty: TypeNode) extends TypeNode with PairElemTypeNode
+
+case class PairTypeNode(fstPET: PairElemTypeNode, sndPET: PairElemTypeNode) extends TypeNode
+
+// PairElemTypeNode
+sealed trait PairElemTypeNode
+
+case class PETPairNode() extends PairElemTypeNode
+
+case class IntLiterNode(n: Int) extends ExprNode
+
+case class BoolLiterNode(b: Boolean) extends ExprNode
+
+case class CharLiterNode(c: Char) extends ExprNode
+
+case class StrLiterNode(s: String) extends ExprNode
+
+case class PairLiterNode() extends ExprNode
+
+case class UnOpExprNode(op: UnaryOperNode, expr: ExprNode) extends ExprNode
+
+case class BinOpExprNode(expr: ExprNode, op: BinaryOperatorNode, expr: ExprNode) extends ExprNode
+
+case class BracketExprNode(expr: ExprNode) extends ExprNode
+
+case class UnaryOperNode(op: String)
+
+case class BinaryOperatorNode(op: String)
