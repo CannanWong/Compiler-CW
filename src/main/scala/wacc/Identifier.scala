53536c315436fbcfbd854a2f2862a5c72ccddc5e
Tsz Wong <tcw121@aube.doc.ic.ac.uk>
2023-02-03 17:40:38 +0000

    [tcw121][lst20] Created Identifier classes for variable, functions and arrays to support the symbol table

--- src/main/scala/wacc/Identifier.scala
+++ src/main/scala/wacc/Identifier.scala
@@ -1,5 +1,12 @@
 package wacc
 
-class Identifier {
-  
-}
+sealed trait Identifier
+
+case class VarIdentifier(ty: String)
+
+case class FuncIdntifier(paramtype: List[String], returntype: String)
+
+case class ArrayIdentifier(ty: String, size: Int, elements: List[Any])
+
+// val a = newArrayIdentifier("int[]", 3, [1,2,3])
+// a.ty == "int[]"
