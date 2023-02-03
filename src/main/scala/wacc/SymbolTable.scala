4062780422c9274642ab38f9d46198a3c2e200a6
Tsz Wong <tcw121@aube.doc.ic.ac.uk>
2023-02-03 17:14:38 +0000

    Merge commits

---
+++ src/main/scala/wacc/SymbolTable.scala
@@ -0,0 +1,34 @@
+package wacc
+
+import scala.collection.mutable
+
+/**
+  * 
+  *
+  */
+
+class SymbolTable {
+
+    val map: mutable.Map[String, Identifier] = mutable.Map()
+    
+    def add(name: String, ident: Identifier): Unit = {
+        map.addOne(name, ident)
+    }
+
+    def lookUp(name: String): Option[Identifier] = {
+        return map.get(name)
+    }
+
+        
+}
+
+
+/*
+    all variables being globally unique
+
+    renaming:
+    <scope index> + ! + variable name
+    scope index tracked using a stack
+    new scope index assignment = max + 1
+*/
+
