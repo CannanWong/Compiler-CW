73a09fd371309351d0bdf0cf730d0fdd6b35263f
Tsz Wong <tcw121@aube.doc.ic.ac.uk>
2023-02-04 19:57:13 +0000

    Added symbol table lookup to SemanticChecker.checkIfWhileCond()

--- src/main/scala/wacc/Identifier.scala
+++ src/main/scala/wacc/Identifier.scala
@@ -2,11 +2,11 @@ package wacc
 
 sealed trait Identifier
 
-case class VarIdentifier(ty: String)
+case class VarIdentifier(ty: String) extends Identifier
 
-case class FuncIdntifier(paramtype: List[String], returntype: String)
+case class FuncIdntifier(paramtype: List[String], returntype: String) extends Identifier
 
-case class ArrayIdentifier(ty: String, size: Int, elements: List[Any])
+case class ArrayIdentifier(ty: String, size: Int, elements: List[Any]) extends Identifier
 
 // val a = newArrayIdentifier("int[]", 3, [1,2,3])
 // a.ty == "int[]"
--- src/main/scala/wacc/SemanticChecker.scala
+++ src/main/scala/wacc/SemanticChecker.scala
@@ -17,12 +17,47 @@ object SemanticChecker {
         expr match {
             case IntLiterNode(_) | CharLiterNode(_) | StrLiterNode(_) | PairLiterNode()
                 => SemanticChecker.errorMessage += "Semantic error in if statement\n"
-            case IdentNode(_)
+            case IdentNode(name)
                 // Check symbol table if variable is of type bool
-                => SemanticChecker.errorMessage += "Semantic error in if statement\n"
-            case ArrayElemNode(_,_)
+                => {
+                    val varName = "v" + scope.toString() + "!" + name
+                    if (SemanticChecker.symbolTable.lookUp(varName).isEmpty) {
+                        SemanticChecker.errorMessage += "Semantic error in if statement\n"
+                    }
+                    else {
+                        val identifier = SemanticChecker.symbolTable.lookUp(varName).get
+                        identifier match {
+                            case VarIdentifier(ty)
+                                => if (ty != "bool") {
+                                    SemanticChecker.errorMessage += "Semantic error in if statement\n"
+                                }
+                            case _
+                               => SemanticChecker.errorMessage += "[Not possible!] Semantic error in if statement\n" 
+                        }
+                    }
+                }
+            case ArrayElemNode(IdentNode(name), exprList)
                 // Check symbol table if array elem is of type bool
-                => SemanticChecker.errorMessage += "Semantic error in if statement\n"
+                => {
+                    val arrayName = "a" + scope.toString() + "!" + name
+                    if (SemanticChecker.symbolTable.lookUp(arrayName).isEmpty) {
+                        SemanticChecker.errorMessage += "Semantic error in if statement\n"
+                    }
+                    else {
+                        val identifier = SemanticChecker.symbolTable.lookUp(arrayName).get
+                        identifier match {
+                            case ArrayIdentifier(ty, _, _)
+                                => if (exprList.length == 1) {
+                                    if (ty != "bool") {
+                                        SemanticChecker.errorMessage += "Semantic error in if statement\n"
+                                    }
+                                }
+                                // else {2+ dimension arrays}
+                            case _
+                               => SemanticChecker.errorMessage += "[Not possible!] Semantic error in if statement\n" 
+                        }
+                    }
+                }
             case UnOpExprNode(UnaryOperNode(op),_)
                 => if (op != "!") {
                         SemanticChecker.errorMessage += "Semantic error in if statement\n"
