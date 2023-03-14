b5c1e63ff3438fcc8ff6497cbaef4023a74a7dfd
Gabriel Lee <gl721@aube.doc.ic.ac.uk>
2023-03-14 23:21:09 +0000

    added optimisation for comparison before div and mod, removed incorrect optimisation for push/pop

--- src/main/scala/wacc/PeepholeOptimisation.scala
+++ src/main/scala/wacc/PeepholeOptimisation.scala
@@ -4,6 +4,7 @@ import scala.collection.mutable._
 import wacc.Constants._
 
 object PeepholeOptimisation {
+    /* Main function to be called, optimises each funcBlock */
     def peepholeOptimise(cfg: LinkedHashMap[String, FuncBlock]): Unit = {
         for ((name, funcBlock) <- cfg) {
             optimiseBlock(funcBlock)
@@ -25,6 +26,33 @@ object PeepholeOptimisation {
         
     }
 
+    def optimiseBlock(ifBlock: IfBlock): Unit = {
+        optimiseBlock(ifBlock.nextT)
+        optimiseBlock(ifBlock.nextF)
+        optimiseBlock(ifBlock.next)
+    }
+
+    def optimiseBlock(whileBlock: WhileBlock): Unit = {
+        optimiseBlock(whileBlock.cond)
+        optimiseBlock(whileBlock.loop)
+        optimiseBlock(whileBlock.next)
+    }
+
+    def optimiseBlock(instBlock: InstBlock): Unit = {
+        val newInstList: ListBuffer[Instruction] = new ListBuffer()
+        val iter = instBlock.instList.iterator
+        while (iter.hasNext) {
+            val inst = iter.next
+            newInstList ++= optimiseInst(inst, iter)  
+        }
+        instBlock.instList = newInstList
+        instBlock.next match {
+            case InstBlock() | IfBlock() | WhileBlock() => optimiseBlock(instBlock.next)
+            case _ => 
+        }
+    }
+
+    /* Returns the first instruction that isn't a redundant move instruction starting from inst */
     def removeRedundant(inst: Instruction, iter: Iterator[Instruction]): Option[Instruction] = {
         inst match {
             case MovInst(rd, op, _) => {
@@ -38,6 +66,8 @@ object PeepholeOptimisation {
         }
     }
 
+    /* Optimises the current instruction given one or more following instructions, 
+       returning a list of optimised instructions */
     def optimiseInst(instruction: Instruction, iter: Iterator[Instruction]): ListBuffer[Instruction] = {
         var optimisedInsts: ListBuffer[Instruction] = new ListBuffer()
         var option = removeRedundant(instruction, iter)
@@ -53,9 +83,11 @@ object PeepholeOptimisation {
             }
         }
 
-        // It has been checked that option is not empty and iter.hasNext is true
+        /* It has been checked that option is not empty and iter.hasNext is true */
         val inst = option.get
         inst match {
+            /* Optimises out a load instruction immediately following a store instruction 
+               for the same register and address */
             case StrInst(rd, op) => {
                 optimisedInsts += inst
                 removeRedundant(iter.next, iter) match {
@@ -76,40 +108,84 @@ object PeepholeOptimisation {
                     case None =>    
                 }
             }
-            case i: PushInst => {
-                var pushInsts: ListBuffer[PushInst] = ListBuffer(i)
-                var optionNext: Option[Instruction] = removeRedundant(iter.next, iter)
+            // case i: PopInst => {
+            //     var popInsts: ListBuffer[PopInst] = ListBuffer(i)
+            //     var optionNext: Option[Instruction] = removeRedundant(iter.next, iter)
 
-                while (optionNext.contains(PushInst)) {
-                    pushInsts += optionNext.get.asInstanceOf[PushInst]
-                    if (iter.hasNext) {
-                        optionNext = removeRedundant(iter.next, iter)
-                    } else {
-                        optionNext = None
-                    }
-                }
+            //     while (optionNext match {
+            //             case Some(PopInst(_*)) => true 
+            //             case _ => false}) {
+            //         popInsts += optionNext.get.asInstanceOf[PopInst]
+            //         if (iter.hasNext) {
+            //             optionNext = removeRedundant(iter.next, iter)
+            //         } else {
+            //             optionNext = None
+            //         }
+            //     }
 
-                var matchingRegs: Boolean = true
-                while (matchingRegs && optionNext.contains(PopInst)) {
-                    if (optionNext.get.asInstanceOf[PopInst].regs == pushInsts.last.regs) {
-                        pushInsts = pushInsts.dropRight(1)
+            //     var matchingRegs: Boolean = true
+            //     while (matchingRegs && 
+            //             (optionNext match {
+            //                 case Some(PushInst(_*)) => true
+            //                 case _ => false
+            //             }) 
+            //             && popInsts.nonEmpty) {
+            //         if (optionNext.get.asInstanceOf[PushInst].regs == popInsts.last.regs) {
+            //             println("match")
+            //             popInsts = popInsts.dropRight(1)
+            //             if (iter.hasNext) {
+            //                 optionNext = removeRedundant(iter.next, iter)
+            //             } else {
+            //                 optionNext = None
+            //             }
+            //         } else {
+            //             println("not match")
+            //             matchingRegs = false
+            //         }
+            //     }
+            //     optimisedInsts ++= popInsts
+            //     optionNext match {
+            //         case Some(next) => {
+            //             optimisedInsts ++= optimiseInst(next, iter)
+            //         }
+            //         case None =>
+            //     }
+            // }
+            /* Optimises the setup before a division or modulo operation, so that if the denominator is an ImmVal, 
+               the compare instruction is removed, if the ImmVal is 0, it will branch directly to the error label, otherwise it will not*/
+            case MovInst(FixedRegister(1), op, _) => {
+                optimisedInsts += inst
+                var next = iter.next
+                next match {
+                    case CmpInst(FixedRegister(1), ImmVal(0)) => {
+                        optimisedInsts += next
                         if (iter.hasNext) {
-                            optionNext = removeRedundant(iter.next, iter)
-                        } else {
-                            optionNext = None
+                            next = iter.next
+                            next match {
+                                case BranchInst(ZERO_DIVISION_LABEL, _, Equal()) => {
+                                    op match {
+                                        case ImmVal(num) => {
+                                            optimisedInsts = optimisedInsts.dropRight(1)
+                                            num match {
+                                                case 0 => {
+                                                    optimisedInsts += BranchInst(ZERO_DIVISION_LABEL)
+                                                }
+                                                case _ => 
+                                            }
+                                        }
+                                        case _ => optimisedInsts += next
+                                    }
+                                }
+                                case _ => optimisedInsts ++= optimiseInst(next, iter)
+                            }
                         }
-                    } else {
-                        matchingRegs = false
                     }
-                }
-                optimisedInsts ++= pushInsts
-                optionNext match {
-                    case Some(next) => {
-                        optimisedInsts ++= optimiseInst(next, iter)
-                    }
-                    case None =>
+                    case _ => optimisedInsts ++= optimiseInst(next, iter)
                 }
             }
+            /* Removes a move instruction that follows a reversed move instruction
+               Eg. Mov r1, r2 followed by Mov r2, r1
+               The second instruction is not needed. */
             case MovInst(rd, op, cond) => {
                 optimisedInsts += inst
                 if (iter.hasNext) {
@@ -129,31 +205,5 @@ object PeepholeOptimisation {
             }
         }
         optimisedInsts
-    }
-    
-    def optimiseBlock(instBlock: InstBlock): Unit = {
-        var newInstList: ListBuffer[Instruction] = new ListBuffer()
-        val iter = instBlock.instList.iterator
-        while (iter.hasNext) {
-            var inst = iter.next
-            newInstList ++= optimiseInst(inst, iter)  
-        }
-        instBlock.instList = newInstList
-        instBlock.next match {
-            case InstBlock() | IfBlock() | WhileBlock() => optimiseBlock(instBlock.next)
-            case _ => 
-        }
-    }
-
-    def optimiseBlock(ifBlock: IfBlock): Unit = {
-        optimiseBlock(ifBlock.nextT)
-        optimiseBlock(ifBlock.nextF)
-        optimiseBlock(ifBlock.next)
-    }
-
-    def optimiseBlock(whileBlock: WhileBlock): Unit = {
-        optimiseBlock(whileBlock.cond)
-        optimiseBlock(whileBlock.loop)
-        optimiseBlock(whileBlock.next)
     }
 }
